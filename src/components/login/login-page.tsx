1a8d0c961b7a0a854793459933316893f89b9d79
Ahmad Badran <ahmad.badran.19999@gmail.com>
2021-01-30 17:17:02 +0200

    changed the style

--- src/components/login/login-page.tsx
+++ src/components/login/login-page.tsx
@@ -17,31 +17,33 @@ export default function LoginPage() {
   };
 
   return (
-    <form className = "form1" onSubmit={login}>
-      <label htmlFor="email">Username</label>
-      <input
-        placeholder="Username"
-        value={email}
-        onChange={(event) =>
-          setCredentials({
-            email: event.target.value,
-            password,
-          })
-        }
-      />
-      <label htmlFor="password">Password</label>
-      <input
-        placeholder="Password"
-        type="password"
-        value={password}
-        onChange={(event) =>
-          setCredentials({
-            email,
-            password: event.target.value,
-          })
-        }
-      />
-      <button type="submit">Login</button>
-    </form>
+    <div className="login-page">
+      <form className="form1" onSubmit={login}>
+        <label htmlFor="email">Username</label>
+        <input
+          placeholder="Username"
+          value={email}
+          onChange={(event) =>
+            setCredentials({
+              email: event.target.value,
+              password,
+            })
+          }
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(event) =>
+            setCredentials({
+              email,
+              password: event.target.value,
+            })
+          }
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
   );
 }
