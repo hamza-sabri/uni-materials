ace07b9a4b47e8c7cd450ebf673c3bd8da12392d
hamza-sabri5 <hamoz.mms@gmail.com>
2021-02-06 01:39:16 +0200

    adding the logic of the urls all in one place

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,7 +1,17 @@
+import { concatenatorInterface } from "../interfaces/concatenator";
+
 // external urls
 const rootURL:string = 'https://us-central1-uni-materials-412a2.cloudfunctions.net/webApi';
 
 // routes
 const signinRoute:string = '/users/signin';
 
-export { rootURL, signinRoute };
+
+
+
+// functions
+const urlConcatenator = ({seconderyPath}:concatenatorInterface): string => {
+    return `${rootURL}${seconderyPath}`;
+};
+
+export { rootURL, signinRoute, urlConcatenator };
