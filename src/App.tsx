5ced8ac89f4e98399e8c2e232e0b4399c50f67e0
hamza-sabri5 <hamoz.mms@gmail.com>
2021-02-06 01:13:58 +0200

    testing the APIs one by one! I hsould see a better way of doing this

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { userCredentials } from './interfaces/user/credentials';
+import { signin } from './requestes/user';
+
 function App() {
-	return <div className="App" />;
+	const test = async() => {
+		const newUser: userCredentials = {
+			email: 'ahmad.badran.19999@gmail.com',
+			password: 'hamoz2000@@xX'
+		};
+		console.log(await signin(newUser));
+	};
+	return (
+		<div className="App">
+			<button onClick={test}>send data</button>
+		</div>
+	);
 }
 
 export default App;
