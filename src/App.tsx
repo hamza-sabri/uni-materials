23b034fa93eea3fc206e0c0ca42cc90b6f94c5e4
hamza-sabri5 <hamoz.mms@gmail.com>
2021-02-07 16:40:36 +0200

    testing site

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import { userCredentials } from './interfaces/user/credentials';
-import { getUserCredentials, signin, signup } from './requestes/user-requestes/user';
+import { APIsCaller } from './requestes/apis-caller';
+import { getAllUnis } from './requestes/uni-requests/university';
+import { signin, signup } from './requestes/user-requestes/user';
 
 function App() {
 	const test = async () => {
+		// const existedUser: userCredentials = {
+		// 	email: 'ahmad.badran.19999@gmail.com',
+		// 	password: 'hamoz2000@@xX'
+		// };
 
-		const existedUser: userCredentials = {
-			email: 'ahmad.badran.19999@gmail.com',
-			password: 'hamoz2000@@xX'
-		};
-		const newUser: userCredentials = {
-			email: 'abohmade20asd2@gmail.com',
-			password: 'hamoz2000@@xX',
-			firstName: 'Ahmad',
-			lastName: 'guze',
-			universityName: 'PTUK',
-			field: 'CSE',
-			studentNumber: '201710163',
-			universityLocation: 'Tulkarem'
-		};
-		console.log(await signin(existedUser));
-		console.log(await signup(newUser));
-		console.log(getUserCredentials());
+		// const newUser: userCredentials = {
+		// 	email: 'abohmade20asd2@gmail.com',
+		// 	password: 'hamoz2000@@xX',
+		// 	firstName: 'Ahmad',
+		// 	lastName: 'guze',
+		// 	universityName: 'PTUK',
+		// 	field: 'CSE',
+		// 	studentNumber: '201710163',
+		// 	universityLocation: 'Tulkarem'
+		// };
+
+		// console.log(await signin(existedUser));
+		// console.log(await signup(newUser));
+		// console.log(getUserCredentials());
+		console.log(await APIsCaller(getAllUnis));
 	};
 	return (
 		<div className="App">
