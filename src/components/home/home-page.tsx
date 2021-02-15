9e4209af69f046f024d7c95efc85514b82f985fc
hamza-sabri5 <hamoz.mms@gmail.com>
2021-02-16 00:34:36 +0200

    creating the testing fundemantuls of the home page layout. this is just for testing and if it worked would be merged with the master branch latter on

--- src/components/home/home-page.tsx
+++ src/components/home/home-page.tsx
@@ -1,6 +1,13 @@
 import { validateUserOrSignHimIn } from '../../utilities/user-checker';
+import React from 'react';
+import NavBar from './nav-bar';
 
+import '../../styles/home/home-style.css'
+import SideBar from './side-bar';
 export default function HomePage() {
 	validateUserOrSignHimIn();
-	return <div>Home page</div>;
+	return <div className='home-page'>
+		<NavBar />
+		<SideBar />
+	</div>;
 }
