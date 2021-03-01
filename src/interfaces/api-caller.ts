3336997502cb01099d8789384e776124c47b713a
hamza-sabri5 <hamoz.mms@gmail.com>
2021-03-01 17:12:07 +0200

    apisCaller addjusting

--- src/interfaces/api-caller.ts
+++ src/interfaces/api-caller.ts
@@ -1,8 +1,5 @@
-import { AxiosInstance } from 'axios';
-import { responseInterface } from './responses';
-
 export type apiCallerInterface = {
 	requestBody?: object;
 	requestParams?: object;
-	api: any; // this must be changed
+	api: any;
 };
