59505b3c8ed17409269503186c82da978c6b53fc
hamza-sabri5 <hamoz.mms@gmail.com>
2021-03-13 00:24:50 +0200

    scrolling

--- src/pages/manual-entry.tsx
+++ src/pages/manual-entry.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import CardCreateor from '../components/home/subpages/data-entry/add-manually';
 
 export default function AddManuallyPage() {
-	const inputs = ['Material Name','Material Image','Material Number']
+	const inputs = ['Material Name','Material Image','Material Number', 'Material Name','Material Image','Material Number']
 	return (
 		<div className="dynamic-subpage">
 			<CardCreateor {...{inputs}}/>
