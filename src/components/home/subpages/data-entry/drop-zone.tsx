d11c2b47e1e4addc59d5f8ffd666c73fdc27e5b4
hamza-sabri5 <hamoz.mms@gmail.com>
2021-03-15 17:01:25 +0200

    adding the drop zone container

--- src/components/home/subpages/data-entry/drop-zone.tsx
+++ src/components/home/subpages/data-entry/drop-zone.tsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
 
 export default function DropZone() {
-    return (<div className="drop-zoon-container"></div>);
+	const onDrop = useCallback((acceptedFiles) => {
+		// Do something with the files
+	}, []);
+	const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
+	return (
+		<div className="drop-zoon-container">
+			<div {...getRootProps()}>
+				<input {...getInputProps()} />
+				{isDragActive ? (<p className='active-p'>Drop here !!!</p>) : 
+                (<p>Click to Add, Or drag and drop a PDF file</p>)}
+			</div>
+		</div>
+	);
 }
