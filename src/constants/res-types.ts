bfb70ea0bec26b914b5c915d4daacbd5d6cdb351
hamza-sabri5 <hamoz.mms@gmail.com>
2021-04-21 01:33:49 +0300

    the types of res that we currently support on our website

---
+++ src/constants/res-types.ts
@@ -0,0 +1,7 @@
+const pdfsType: string = 'PDF';
+const videosType: string = 'Videos';
+const questionAndAnswerType: string = 'Q&A';
+const usefulResType: string = 'Resources';
+const rulesType: string = 'Rules';
+
+export { pdfsType, videosType, questionAndAnswerType, usefulResType, rulesType };
