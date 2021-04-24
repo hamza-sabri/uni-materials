c7c41ba4f63e560a97ea5fcc78d2b4d097ce8c5e
hamza-sabri5 <hamoz.mms@gmail.com>
2021-04-24 23:49:56 +0300

    getting data from the url

--- src/components/home/subpages/data-entry/res-adders/res-adders.tsx
+++ src/components/home/subpages/data-entry/res-adders/res-adders.tsx
@@ -5,16 +5,17 @@ import qAndA from '../../../../../assets/data-entry-assets/Q-and-A.json';
 import rulesAnimation from '../../../../../assets/data-entry-assets/rules.json';
 import usefulResAnimation from '../../../../../assets/data-entry-assets/useful-resources.json';
 import videoAnimatedIcon from '../../../../../assets/data-entry-assets/video-animated-icon.json';
+import defualtWebsiteImage from '../../../../../assets/data-entry-assets/website.webp';
 import DropZone from '../drop-zone';
 import lottie from 'lottie-web';
 import Swal from 'sweetalert2';
 import { APIsCaller } from '../../../../../requestes/apis-caller';
 import { createNewRes } from '../../../../../requestes/res-requests/res';
-import { pdfsType, questionAndAnswerType, videosType } from '../../../../../constants/res-types';
-import { hideLoading, showLoading } from '../../../../../utilities/alearts';
+import { pdfsType, questionAndAnswerType, rulesType, usefulResType, videosType } from '../../../../../constants/res-types';
+import { showLoading } from '../../../../../utilities/alearts';
 import { CREATED, OK } from '../../../../../constants/status-codes';
 import youtubeImage from '../../../../../assets/data-entry-assets/youtube.jpg';
-import * as ytdl from 'ytdl-core';
+import axios from 'axios';
 
 export function PDFAdder({ matID, topicID }: { matID: string; topicID: string }) {
 	const results: string[] = [ '', '', '' ];
@@ -72,6 +73,7 @@ export function PDFAdder({ matID, topicID }: { matID: string; topicID: string })
 export function VideoAdder({ matID, topicID }: { matID: string; topicID: string }) {
 	const divRef = useRef<HTMLDivElement>(null);
 	const videoRef = useRef<HTMLInputElement>(null);
+	const [ name, setName ] = useState<string>('???????');
 	const [ imgURl, setImgURL ] = useState<string>(youtubeImage);
 	const submitHandler = async () => {
 		const VideoLink: string = videoRef.current!.value;
@@ -83,8 +85,9 @@ export function VideoAdder({ matID, topicID }: { matID: string; topicID: string 
 			};
 			const requestBody = {
 				resType: videosType,
-				fileName: "video title", //TODO get the title of the video some how
-				link: VideoLink
+				videoName: name, //TODO get the title of the video some how
+				link: VideoLink,
+				videoImage: imgURl
 			};
 			showLoading(0);
 			const { status, data } = await APIsCaller({ api: createNewRes, requestParams, requestBody });
@@ -93,17 +96,18 @@ export function VideoAdder({ matID, topicID }: { matID: string; topicID: string 
 			else Swal.fire('Ops!', data.message, 'error');
 		}
 	};
-	const getVideoID = () => {
-		const link = videoRef.current!.value || '';
-		const firstAndIndex: number = link.indexOf('&');
-		const idIndex: number = link.indexOf('=') + 1;
-		return link.substring(idIndex, firstAndIndex === -1 ? link.length : firstAndIndex);
-	};
 
-	const changeVideCard = () => {
-		const videoID = getVideoID();
-		if (videoID === '' || videoID.length < 3) setImgURL(youtubeImage);
-		else setImgURL(`https://img.youtube.com/vi/${videoID}/maxresdefault.jpg`);
+	const changeVideCard = async () => {
+		const url: string = videoRef.current!.value || '';
+		const { data } = await axios.get(`http://api.linkpreview.net/?key=d71742f9c4c457e68276f6288fdfc4d0&q=${url}`);
+		const { image, title } = data;
+		if (image) {
+			setImgURL(image);
+			setName(title);
+		} else {
+			setImgURL(youtubeImage);
+			setName('???????');
+		}
 	};
 
 	useEffect(() => {
@@ -130,6 +134,9 @@ export function VideoAdder({ matID, topicID }: { matID: string; topicID: string 
 			/>
 			<div className="video-previewer">
 				<img src={imgURl} alt="youtube" />
+				<div className="name-wrapper">
+					<div className="name">{name}</div>
+				</div>
 			</div>
 			<div className="res-submit-btn" onClick={submitHandler}>
 				submit
@@ -138,9 +145,47 @@ export function VideoAdder({ matID, topicID }: { matID: string; topicID: string 
 	);
 }
 
-export function UsefulRes() {
-	const divRef = useRef<HTMLInputElement>(null);
-	const submitHandler = () => {};
+export function UsefulRes({ matID, topicID }: { matID: string; topicID: string }) {
+	const divRef = useRef<HTMLDivElement>(null);
+	const urlRef = useRef<HTMLInputElement>(null);
+	const [ name, setName ] = useState<string>('???????');
+	const [ imgURl, setImgURL ] = useState<string>(defualtWebsiteImage);
+
+	const changeVideCard = async () => {
+		const url: string = urlRef.current!.value || '';
+		const { data } = await axios.get(`http://api.linkpreview.net/?key=d71742f9c4c457e68276f6288fdfc4d0&q=${url}`);
+		const { image, title } = data;
+		if (image) {
+			setImgURL(image);
+			setName(title);
+		} else {
+			setImgURL(defualtWebsiteImage);
+			setName('???????');
+		}
+	};
+
+	const submitHandler = async () => {
+		const websiteLink: string = urlRef.current!.value;
+		if (websiteLink === '') Swal.fire('Ops!', 'Sorry but all fields must not be empty', 'error');
+		else {
+			const requestParams = {
+				materialID: matID,
+				topicID: topicID
+			};
+			const requestBody = {
+				resType: usefulResType,
+				websiteName: name, //TODO get the title of the video some how
+				link: websiteLink,
+				websiteImage: imgURl
+			};
+			showLoading(0);
+			const { status, data } = await APIsCaller({ api: createNewRes, requestParams, requestBody });
+			console.log(status, data);
+			if (status === OK || status === CREATED) Swal.fire('Congrats', data.message, 'success');
+			else Swal.fire('Ops!', data.message, 'error');
+		}
+	};
+
 	useEffect(() => {
 		lottie
 			.loadAnimation({
@@ -155,8 +200,20 @@ export function UsefulRes() {
 	return (
 		<div className="adder">
 			<div className="res-animation-container" ref={divRef} />
-			<input type="text" className="res-input" placeholder="Resource link" />
-			<div className="video-previewer" />
+			<input
+				type="text"
+				className="res-input"
+				placeholder="Resource link"
+				ref={urlRef}
+				onChange={changeVideCard}
+				onBlur={changeVideCard}
+			/>
+			<div className="video-previewer">
+				<img src={imgURl} alt="youtube" />
+				<div className="name-wrapper">
+					<div className="name">{name}</div>
+				</div>
+			</div>
 			<div className="res-submit-btn" onClick={submitHandler}>
 				submit
 			</div>
@@ -180,7 +237,7 @@ export function QAAdder({ matID, topicID }: { matID: string; topicID: string }) 
 			const requestBody = {
 				resType: questionAndAnswerType,
 				question,
-				answer,
+				answer
 			};
 			showLoading(0);
 			const { status, data } = await APIsCaller({ api: createNewRes, requestParams, requestBody });
@@ -203,8 +260,8 @@ export function QAAdder({ matID, topicID }: { matID: string; topicID: string }) 
 	return (
 		<div className="adder">
 			<div className="res-animation-container" ref={divRef} />
-			<textarea className="res-text-area" placeholder="Question" ref={questionRef}/>
-			<textarea className="res-text-area" placeholder="Answer" ref={answerRef}/>
+			<textarea className="res-text-area" placeholder="Question" ref={questionRef} />
+			<textarea className="res-text-area" placeholder="Answer" ref={answerRef} />
 			<div className="res-submit-btn" onClick={submitHandler}>
 				submit
 			</div>
@@ -212,9 +269,35 @@ export function QAAdder({ matID, topicID }: { matID: string; topicID: string }) 
 	);
 }
 
-export function Rules() {
-	const divRef = useRef<HTMLInputElement>(null);
-	const submitHandler = () => {};
+export function Rules({ matID, topicID }: { matID: string; topicID: string }) {
+	const divRef = useRef<HTMLDivElement>(null);
+	const lawNameRef = useRef<HTMLInputElement>(null);
+	const lawConentRef = useRef<HTMLInputElement>(null);
+	const lawExampleRef = useRef<HTMLInputElement>(null);
+	const submitHandler = async () => {
+		const lawName: string = lawNameRef.current!.value;
+		const lawConent: string = lawConentRef.current!.value;
+		const lawExample: string = lawExampleRef.current!.value;
+		if (lawName === '' || lawConent === '' || lawExample === '')
+			Swal.fire('Ops!', 'Sorry but all fields must not be empty', 'error');
+		else {
+			const requestParams = {
+				materialID: matID,
+				topicID: topicID
+			};
+			const requestBody = {
+				resType: rulesType,
+				lawName,
+				lawConent,
+				lawExample
+			};
+			showLoading(0);
+			const { status, data } = await APIsCaller({ api: createNewRes, requestParams, requestBody });
+			console.log(status, data);
+			if (status === OK || status === CREATED) Swal.fire('Congrats', data.message, 'success');
+			else Swal.fire('Ops!', data.message, 'error');
+		}
+	};
 	useEffect(() => {
 		lottie
 			.loadAnimation({
