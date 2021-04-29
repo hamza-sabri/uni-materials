c1b884964fe50268eab4df6e0b2b27e2eb1919ca
hamza-sabri5 <hamoz.mms@gmail.com>
2021-04-30 01:04:58 +0300

    adding a loading screan if the data of the profile is not thier yet

--- src/components/home/subpages/user-profile/profile.tsx
+++ src/components/home/subpages/user-profile/profile.tsx
@@ -1,17 +1,38 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import Avatar from './avatar';
+import lottie from 'lottie-web';
 import '../../../../styles/dynamic-content/user-profile/user-profile.css';
+import loadingProfileAnim from '../../../../assets/home/profile/loading-profile.json'
 
 import { DynamicContentContext } from '../../../../contexts/home-context/dynamic-content-state-context';
 
 export default function Profile() {
 	const { unisDataList, user, setUser } = useContext(DynamicContentContext);
+	const loadingRef = useRef<HTMLDivElement>(null);
 
 	// replace the empty div with a loading container
 
+	useEffect(() => {
+		lottie.loadAnimation({
+			container: loadingRef.current!,
+			autoplay: true,
+			renderer: 'svg',
+			loop: true,
+			animationData: loadingProfileAnim
+		});
+	}, []);
+
+	const LoadingProfile = ()=>{
+		return (
+			<div className="user-profile-loading">
+				<div className="profile-loading" ref={loadingRef} />
+			</div>
+		)
+	}
+
 	return (
 		<div>
-			{!user.userProfile? <div></div>: <Avatar data={user} unisDataList={unisDataList} setUser={setUser} />}
+			{!user.userProfile? <LoadingProfile />: <Avatar data={user} unisDataList={unisDataList} setUser={setUser} />}
 			
 		</div>
 	);
